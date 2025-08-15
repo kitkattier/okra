e362daba3ca5064baacd5ad5548e55868cc1db8e
kitkattier <arora.krit@gmail.com>
2025-08-15 22:44:33 +1000

    Tamagui config initialisation

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,19 +1,31 @@
-import { Text, View, Image } from "react-native";
+import {
+  createTamagui,
+  TamaguiProvider,
+  View,
+  Image,
+  Text,
+  Button,
+} from "tamagui";
+import { defaultConfig } from "@tamagui/config/v4";
+
+const config = createTamagui(defaultConfig);
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Welcome to Meowlytics!</Text>
-      <Image
-        source={require("../assets/images/meowl.png")}
-        style={{ width: 150, height: 300, marginTop: 200 }}
-      />
-    </View>
+    <TamaguiProvider config={config}>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>Welcome to Meowlytics!</Text>
+        <Image
+          source={require("../assets/images/meowl.png")}
+          style={{ width: 150, height: 300, marginTop: 200 }}
+        />
+      </View>
+    </TamaguiProvider>
   );
 }
