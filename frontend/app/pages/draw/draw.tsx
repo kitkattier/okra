7147ccdd478988dbad1727dd4f8aa5c2d411a2bd
goanna5 <143087684+goanna5@users.noreply.github.com>
2025-08-16 19:44:01 +1000

    colour picker (still need to do transparent)

--- frontend/app/pages/draw/draw.tsx
+++ frontend/app/pages/draw/draw.tsx
@@ -1,11 +1,18 @@
 import MapComponent from "~/components/map";
 import NavBar from "~/components/navBar";
+import ColourPicker from "~/components/colourPicker";
 
 const Draw: React.FC = () => {
   return (
-    <div className="min-h-screen py-3">
+    <div className="flex min-h-screen flex-col py-3">
       <NavBar />
-      <MapComponent />
+      <div className="relative flex-1">
+        <MapComponent />
+
+        <div className="absolute top-4 right-4 z-10 rounded-md bg-white/80 p-3 shadow-md backdrop-blur-md">
+          <ColourPicker />
+        </div>
+      </div>
     </div>
   );
 };
